f3f3aea97fc0372e926e3c2f305c4c904cb61e6b
Mubtasim Fuad <m.fuad.rafi@gmail.com>
2021-04-20 21:29:20 +0600

    Update suvat_calculator_en.py

--- suvat_calculator_en.py
+++ suvat_calculator_en.py
@@ -40,11 +40,11 @@ elif sum(known_variables.values()) > 3:
 
 if check_variables(known_variables, ['u', 'v', 't']):    # ['Initial Velocity (u)', 'Final Velocity (v)', 'Time (t)']
     u, v, t = ask('Initial Velocity (u)', 'Final Velocity (v)', 'Time (t)')
-    if st.button('Click here to calculate and check the necessary equations') is True:
-        st.write("""        The equations which are used to determine the values:
-        $$
-        s = \\frac{1}{2}(u+v)t, \\quad a = \\frac{v-u}{t}
-        $$      """)
+    if st.button('Click here to get the values of the rest two variables with equations') is True:
+        st.write('The equations which are used to determine the values:\n' +
+        '$$\n' +
+        's = \\frac{1}{2}(u+v)t, \\quad a = \\frac{v-u}{t}' +
+        '$$')
         s = 0.5*(u+v)*t
         try:
             a = (v-u)/t
@@ -57,11 +57,11 @@ if check_variables(known_variables, ['u', 'v', 't']):    # ['Initial Velocity (u
 
 elif check_variables(known_variables, ['u', 'v', 'a']):   # ['Initial Velocity (u)', 'Final Velocity (v)', 'Acceleration (a)']
     u, v, a = ask('Initial Velocity (u)', 'Final Velocity (v)', 'Acceleration (a)')
-    if st.button('Click here to calculate and check the necessary equations') is True:
-        st.write("""        The equations which are used to determine the values:
-        $$
-        s = \\frac{v^2-u^2}{2a}, \\quad t = \\frac{v-u}{a}
-        $$      """)
+    if st.button('Click here to get the values of the rest two variables with equations') is True:
+        st.write('The equations which are used to determine the values:\n' +
+        '$$\n' +
+        's = \\frac{v^2-u^2}{2a}, \\quad t = \\frac{v-u}{a}' +
+        '$$')
         try:
             s = (v*v - u*u)/(2*a)
             t = (v - u)/a
@@ -75,11 +75,11 @@ elif check_variables(known_variables, ['u', 'v', 'a']):   # ['Initial Velocity (
 
 elif check_variables(known_variables, ['u', 'a', 't']):  # ['Initial Velocity (u)', 'Acceleration (a)', 'Time (t)']
     u, a, t = ask('Initial Velocity (u)', 'Acceleration (a)', 'Time (t)')
-    if st.button('Click here to calculate and check the necessary equations') is True:
-        st.write("""        The equations which are used to determine the values:
-        $$
-        s = ut + \\frac{1}{2}at^2, \\quad v = u + at
-        $$      """)
+    if st.button('Click here to get the values of the rest two variables with equations') is True:
+        st.write('The equations which are used to determine the values:\n' +
+        '$$\n' +
+        's = ut + \\frac{1}{2}at^2, \\quad v = u + at' +
+        '$$')
         s = u*t + 0.5*a*t*t
         v = u + a*t
         st.write('Inserting your given values in these equations, we get: ')
@@ -88,11 +88,11 @@ elif check_variables(known_variables, ['u', 'a', 't']):  # ['Initial Velocity (u
 
 elif check_variables(known_variables, ['v', 'a', 't']):   # ['Final Velocity (v)', 'Acceleration (a)', 'Time (t)']
     v, a, t = ask('Final Velocity (v)', 'Acceleration (a)', 'Time (t)')
-    if st.button('Click here to calculate and check the necessary equations') is True:
-        st.write("""        The equations which are used to determine the values:
-        $$
-        s = vt - \\frac{1}{2}at^2, \\quad u= v - at
-        $$      """)
+    if st.button('Click here to get the values of the rest two variables with equations') is True:
+        st.write('The equations which are used to determine the values:\n' +
+        '$$\n' +
+        's = vt - \\frac{1}{2}at^2, \\quad u= v - at' +
+        '$$')
         s = v*t - 0.5*a*t*t
         u = v - a*t
         st.write('Inserting your given values in these equations, we get: ')
@@ -101,11 +101,11 @@ elif check_variables(known_variables, ['v', 'a', 't']):   # ['Final Velocity (v)
 
 elif check_variables(known_variables, ['s', 'a', 't']):   # ['Displacement (s)', 'Acceleration (a)', 'Time (t)']
     s, a, t = ask('Displacement (s)', 'Acceleration (a)', 'Time (t)')
-    if st.button('Click here to calculate and check the necessary equations') is True:
-        st.write("""        The equations which are used to determine the values:
-        $$
-        u = \\frac{s}{t} - \\frac{1}{2}at, \\quad v = \\frac{s}{t} + \\frac{1}{2}at
-        $$      """)
+    if st.button('Click here to get the values of the rest two variables with equations') is True:
+        st.write('The equations which are used to determine the values:\n' +
+        '$$\n' +
+        'u = \\frac{s}{t} - \\frac{1}{2}at, \\quad v = \\frac{s}{t} + \\frac{1}{2}at' +
+        '$$')
         try:
             u = (s - 0.5*a*t*t)/t
             v = (s + 0.5*a*t*t)/t
@@ -119,11 +119,11 @@ elif check_variables(known_variables, ['s', 'a', 't']):   # ['Displacement (s)',
 
 elif check_variables(known_variables, ['s', 'v', 't']):   # ['Displacement (s)', 'Final Velocity (v)', 'Time (t)']
     s, v, t = ask('Displacement (s)', 'Final Velocity (v)', 'Time (t)')
-    if st.button('Click here to calculate and check the necessary equations') is True:
-        st.write("""        The equations which are used to determine the values:
-        $$
-        u = \\frac{2s}{t} - v, \\quad a = \\frac{2(vt-s)}{t^2}
-        $$      """)
+    if st.button('Click here to get the values of the rest two variables with equations') is True:
+        st.write('The equations which are used to determine the values:\n' +
+        '$$\n' +
+        'u = \\frac{2s}{t} - v, \\quad a = \\frac{2(vt-s)}{t^2}' +
+        '$$')
         try:
             u = (2*s)/t - v
             a = 2*(v*t-s)/(t*t)
@@ -137,11 +137,11 @@ elif check_variables(known_variables, ['s', 'v', 't']):   # ['Displacement (s)',
 
 elif check_variables(known_variables, ['s', 'v', 'a']):   # ['Displacement (s)', 'Final Velocity (v)', 'Acceleration (a)']
     s, v, a = ask('Displacement (s)', 'Final Velocity (v)', 'Acceleration (a)')
-    if st.button('Click here to calculate and check the necessary equations') is True:
-        st.write("""        The equations which are used to determine the values:
-        $$
-        u = \\sqrt{v^2 -2as}, \\quad t = \\frac{v}{a} - \\frac{\\sqrt{v^2 - 2as}}{a}
-        $$      """)
+    if st.button('Click here to get the values of the rest two variables with equations') is True:
+        st.write('The equations which are used to determine the values:\n' +
+        '$$\n' +
+        'u = \\sqrt{v^2 -2as}, \\quad t = \\frac{v}{a} - \\frac{\\sqrt{v^2 - 2as}}{a}' +
+        '$$')
         try:
             u = math.sqrt(v*v - 2*a*s)
             t = v/a - math.sqrt(v*v - 2*a*s)/a
@@ -158,11 +158,11 @@ elif check_variables(known_variables, ['s', 'v', 'a']):   # ['Displacement (s)',
 
 elif check_variables(known_variables, ['s', 'u', 't']):   # ['Displacement (s)', 'Initial Velocity (u)', 'Time (t)']
     s, u, t = ask('Displacement (s)', 'Initial Velocity (u)', 'Time (t)')
-    if st.button('Click here to calculate and check the necessary equations') is True:
-        st.write("""        The equations which are used to determine the values:
-        $$
-        v = \\frac{2s}{t}-u, \\quad a = \\frac{2(s-ut)}{t^2}
-        $$      """)
+    if st.button('Click here to get the values of the rest two variables with equations') is True:
+        st.write('The equations which are used to determine the values:\n' +
+        '$$\n' +
+        'v = \\frac{2s}{t}-u, \\quad a = \\frac{2(s-ut)}{t^2}' +
+        '$$')
         try:
             v = (2*s)/t - u
             a = 2*(s-u*t)/(t*t)
@@ -176,11 +176,11 @@ elif check_variables(known_variables, ['s', 'u', 't']):   # ['Displacement (s)',
 
 elif check_variables(known_variables, ['s', 'u', 'a']):   # ['Displacement (s)', 'Initial Velocity (u)', 'Acceleration (a)']
     s, u, a = ask('Displacement (s)', 'Initial Velocity (u)', 'Acceleration (a)')
-    if st.button('Click here to calculate and check the necessary equations') is True:
-        st.write("""        The equations which are used to determine the values:
-        $$
-        v = \\sqrt{u^2 + 2as}, \\quad t = -\\frac{u}{a} + \\frac{\\sqrt{u^2 + 2as}}{a}
-        $$      """)
+    if st.button('Click here to get the values of the rest two variables with equations') is True:
+        st.write('The equations which are used to determine the values:\n' +
+        '$$\n' +
+        'v = \\sqrt{u^2 + 2as}, \\quad t = -\\frac{u}{a} + \\frac{\\sqrt{u^2 + 2as}}{a}' +
+        '$$')
         try:
             v = math.sqrt(u*u + 2*a*s)
             t = -u/a + math.sqrt(u*u +2*a*s)/a
@@ -197,11 +197,11 @@ elif check_variables(known_variables, ['s', 'u', 'a']):   # ['Displacement (s)',
 
 elif check_variables(known_variables, ['s', 'u', 'v']):   # ['Displacement (s)', 'Initial Velocity (u)', 'Final Velocity (v)']
     s, u, v = ask('Displacement (s)', 'Initial Velocity (u)', 'Final Velocity (v)')
-    if st.button('Click here to calculate and check the necessary equations') is True:
-        st.write("""        The equations which are used to determine the values:
-        $$
-        a = \\frac{v^2 - u^2}{2s}, \\quad t = \\frac{2s}{u+v}
-        $$      """)
+    if st.button('Click here to get the values of the rest two variables with equations') is True:
+        st.write('The equations which are used to determine the values:\n' +
+        '$$\n' +
+        'a = \\frac{v^2 - u^2}{2s}, \\quad t = \\frac{2s}{u+v}' +
+        '$$')
         try:
             a = (v*v - u*u)/(2*s)
         except ZeroDivisionError:
@@ -210,7 +210,7 @@ elif check_variables(known_variables, ['s', 'u', 'v']):   # ['Displacement (s)',
         try:
             t = (2*s)/(u+v)
         except ZeroDivisionError:
-            st.write('Here $u=v=0$ is not allowed.')
+            st.write('Here $u+v=0$ is not allowed.')
             t = None
         st.write('Inserting your given values in these equations, we get: ')
         st.write('Acceleration $(a) = \\frac{v^2 - u^2}{2s} =$ ', a)
